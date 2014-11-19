597202342aa73af5eacce8e2a16a758bc9a2627c
Thomas Jannaud <jannaud@bleezworld.com>
2014-11-19 18:18:42 +0100

    add comment

--- serveur/user_handler.py
+++ serveur/user_handler.py
@@ -6,7 +6,8 @@ from serveur.db import data_models
 class FLUser:
     """Wrapper class around our User datamodel in the protobuf.
     Flask needs this class to save cookie for session auto-login, ...
-    All the info about the user is saved there."""
+    All the info about the user (first name, ...) is saved in the proto, and all the info
+    about anonymous user, logged in, ... is in the wrapper layer."""
     def __init__(self, user_pb):
         self.user_pb = user_pb
     def is_authenticated(self):
@@ -20,6 +21,7 @@ class FLUser:
 
 
 def load_user(userid):
+    """Used by Flask to make a FLUser from our user id, for auto login sessions from cookie etc."""
     table = data_models.GetTable(data_models.RW_USERS)
     user_pb = data_models.ToProto(table.find_one({"_id": userid}), data_models.RW_USERS)
     if not user_pb: return None
@@ -27,6 +29,8 @@ def load_user(userid):
 
 
 def register():
+    """Creates the user in the database. For users, don't use mongodb's id, e create our own (email, ...).
+    TODO: Should of course use the request params, not harcoded values."""
     user_pb = data_models.all_pbs.User()
     user_pb.id = '1234'
     user_pb.prenom = 'thomas'
@@ -35,11 +39,13 @@ def register():
 
 
 def login():
+    """ login as our user.
+    TODO: in practice, check request login/password."""
     table = data_models.GetTable(data_models.RW_USERS)
     user_pb = data_models.ToProto(table.find_one({"_id": "1234"}), data_models.RW_USERS)
     user = FLUser(user_pb)
     flogin.login_user(user, remember=True)
-    session['toto'] = 2
+    session['toto'] = 0  # this is for playing in /test_user only
     return 'log in success'
 
 
@@ -49,6 +55,9 @@ def logout():
 
 
 def test():
+    """This is for playing with session/login/logout. Sessions exist for anonymous users. Ex: buying goods in an ecommerce
+    -> you need session to know what is in the shopping cart.
+    TODO: in practice, check request login/password."""
     if 'toto' not in session:
         us = data_models.all_pbs.User()
         us.prenom = 'skfjldkjf'
@@ -57,7 +66,6 @@ def test():
         us = data_models.protobuf_json.json2pb(data_models.all_pbs.User(), session['toto'])
         return 'anonyme' + us.prenom
     else:
-        import pdb; pdb.set_trace()
         us = data_models.protobuf_json.json2pb(data_models.all_pbs.User(), session['toto'])
         return 'salut' + flogin.current_user.user_pb.prenom + us.prenom
 
