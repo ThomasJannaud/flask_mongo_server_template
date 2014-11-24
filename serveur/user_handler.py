032abffba44a82c13bae938591f4b5ce554871d9
Thomas Jannaud <jannaud@bleezworld.com>
2014-11-24 14:09:49 +0100

    fix user

--- serveur/user_handler.py
+++ serveur/user_handler.py
@@ -28,10 +28,14 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(userid):
     """Used by Flask to make a FLUser from our user id, for auto login sessions from cookie etc."""
-    table = data_models.GetTable(data_models.RW_USERS)
-    user_pb = data_models.ToProto(table.find_one({"_id": userid}), data_models.RW_USERS)
-    if not user_pb: return None
-    return FLUser(user_pb)
+    try:
+        user_id = int(userid)    
+        table = data_models.GetTable(data_models.RW_USERS)
+        user_pb = data_models.ToProto(table.find_one({"_id": user_id}), data_models.RW_USERS)
+        if not user_pb: return None
+        return FLUser(user_pb)
+    except:
+        return None
 
 
 @app.route('/api/v1/register')
@@ -39,8 +43,8 @@ def register():
     """Creates the user in the database. For users, don't use mongodb's id, you must create our own (email, ...).
     TODO: Should of course use the request params, not harcoded values."""
     user_pb = data_models.all_pbs.User()
-    user_pb.id = '1234'
-    user_pb.prenom = 'thomas'
+    user_pb.id = 1234
+    user_pb.email = "test@mail.com"
     data_models.SaveProto(user_pb, data_models.RW_USERS)
     return 'ok, registered'
 
@@ -50,7 +54,7 @@ def login():
     """ login as our user.
     TODO: in practice, check request login/password."""
     table = data_models.GetTable(data_models.RW_USERS)
-    user_pb = data_models.ToProto(table.find_one({"_id": "1234"}), data_models.RW_USERS)
+    user_pb = data_models.ToProto(table.find_one({"_id": 1234}), data_models.RW_USERS)
     user = FLUser(user_pb)
     flogin.login_user(user, remember=True)
     return 'log in success'
@@ -70,13 +74,13 @@ def test():
     KEY_USER_IN_CREATION = 'user_in_creation'
     if KEY_USER_IN_CREATION not in session:
         user_in_creation = data_models.all_pbs.User()
-        user_in_creation.prenom = 'session prenom'
+        user_in_creation.email = 'anonymous@session'
         session[KEY_USER_IN_CREATION] = data_models.protobuf_json.pb2json(user_in_creation)
     if flogin.current_user.is_anonymous():
         user_in_creation = data_models.protobuf_json.json2pb(data_models.all_pbs.User(), session[KEY_USER_IN_CREATION])
-        return 'anonyme ; current user in creation :' + user_in_creation.prenom
+        return 'anonyme ; current user in creation :' + user_in_creation.email
     else:
         user_in_creation = data_models.protobuf_json.json2pb(data_models.all_pbs.User(), session[KEY_USER_IN_CREATION])
         return 'logged in / not anononymous ; user:%s, current user in creation:%s' % (
-            flogin.current_user.user_pb.prenom, user_in_creation.prenom)
+            flogin.current_user.user_pb.email, user_in_creation.email)
 
