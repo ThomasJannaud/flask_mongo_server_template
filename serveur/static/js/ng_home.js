716f76d4d6248ed94fbf7f6deb1db6de063c6515
Thomas Jannaud <jannaud@bleezworld.com>
2015-02-17 00:34:11 +0100

    everything working fine

--- serveur/static/js/ng_home.js
+++ serveur/static/js/ng_home.js
@@ -9,6 +9,31 @@ ngApp.controller('ngController', ['$scope', '$http', function($scope, $http) {
     // At any time, at most one of them is true.
     $scope.isLogin = false;
     $scope.isSignup = false;
+    $scope.isLoading = false;
+
+    $scope.closeSignup = function() {
+        $scope.isSignup = false;
+        $scope.isLogin = false;
+        ga('send', 'event', 'click', 'close-signup');
+    }
+
+    $scope.closeLogin = function() {
+        $scope.isSignup = false;
+        $scope.isLogin = false;
+        ga('send', 'event', 'click', 'close-login');
+    }
+
+    $scope.showLogin = function() {
+        $scope.isSignup = false;
+        $scope.isLogin = true;
+        ga('send', 'event', 'click', 'show-login');
+    }
+
+    $scope.showSignup = function() {
+        $scope.isLogin = false;
+        $scope.isSignup = true;
+        ga('send', 'event', 'click', 'show-signup');
+    }
 
     // ------------- Signup controller ---------------
     $scope.signup = {
@@ -26,22 +51,32 @@ ngApp.controller('ngController', ['$scope', '$http', function($scope, $http) {
         exp_month: 1,
         exp_year: 2015
     }
+    $scope.monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    $scope.yearList = [2015, 2016, 2017, 2018];
+
 
     $scope.selectMonth = function(index) {
         $scope.card.exp_month = index + 1;
+        ga('send', 'event', 'select', 'register-card-year', $scope.card.exp_month);
     };
 
     $scope.selectYear = function(index) {
         $scope.card.exp_year = 2015 + index;
+        ga('send', 'event', 'select', 'register-card-year', $scope.card.exp_year);
     };
 
     $scope.trySignup = function() {
         // does email already exist ? if not, try create stripe token.
+        $scope.isLoading = true;
+        ga('send', 'event', 'click', 'trySignup');
         $http.get("/api/v1/check-new-email?email=" + $scope.signup.user_info.email)
             .error(function(data, status, headers, config) {
+                $scope.isLoading = false;
+                ga('send', 'event', 'click', 'trySignup', 'error-mail');
                 alert('An account already exists for this email.');
             })
             .success(function(data, status, headers, config) {
+                ga('send', 'event', 'click', 'trySignup', 'success-mail');
                 // get stripe single use token.
                 // on success of stripe response : finalize signup, else say we have bad card number.
                 Stripe.card.createToken($scope.card, 
@@ -49,14 +84,22 @@ ngApp.controller('ngController', ['$scope', '$http', function($scope, $http) {
                         // Stripe Token creation callback. If successful, tries to register on our
                         // website. If registration successful, redirect to /products.
                         if (response.error) {
+                            $scope.isLoading = false;
+                            $scope.$apply();
+                            ga('send', 'event', 'click', 'trySignup', 'error-stripe');
                             alert(response.error.message);
                         } else {
+                            ga('send', 'event', 'click', 'trySignup', 'success-stripe');
                             $scope.signup.stripe_token = response.id;
                             $http.post("/api/v1/register", $scope.signup)
                                 .success(function(jsonData, status, headers, config) {
-                                    window.location.href = "/products";
+                                    $scope.isLoading = false;
+                                    ga('send', 'event', 'click', 'trySignup', 'success-final');
+                                    window.location.href = "/dashboard";
                                 })
                                 .error(function(jsonData, status, headers, config) {
+                                    $scope.isLoading = false;
+                                    ga('send', 'event', 'click', 'trySignup', 'error-final');
                                     alert('Error in account creation. Please contact us if you have any problem : info@opinionazer.com');
                                 });
                         }
@@ -70,14 +113,17 @@ ngApp.controller('ngController', ['$scope', '$http', function($scope, $http) {
         email: "",
         password: "",
         remember: false        
-    }
+    };
 
     $scope.tryLogin = function() {
+        $scope.isLoading = true;
         $http.post("/api/v1/login", $scope.login)
             .success(function(jsonData, status, headers, config) {
-                window.location.href = "/products";
+                $scope.isLoading = false;
+                window.location.href = "/dashboard";
             })
             .error(function(jsonData, status, headers, config) {
+                $scope.isLoading = false;
                 alert('Cant connect. Sign up, use the forget your password functionality or contact us if you have any problem.');
             });
     }
@@ -88,11 +134,14 @@ ngApp.controller('ngController', ['$scope', '$http', function($scope, $http) {
             alert('Email missing');
             return;
         }
+        $scope.isLoading = true;
         $http.get("/api/v1/forgot_password?email=" + $scope.login.email)
             .success(function(jsonData, status, headers, config) {
+                $scope.isLoading = false;
                 alert('Email succesfully sent');
             })
             .error(function(jsonData, status, headers, config) {
+                $scope.isLoading = false;
                 alert('Could not send the password to the specified email. Account may not exist.');
             });
     }
--- serveur/static_handler.py
+++ serveur/static_handler.py
@@ -2,6 +2,7 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from serveur import app
+from serveur import Constants
 
 
 @app.errorhandler(404)
@@ -12,4 +13,10 @@ def page_not_found(e):
 @app.route('/', methods=['GET'])
 def page_home():
     """Home page any language."""
-    return render_template('home.html')
+    return render_template('home.html', stripe_publishable_key=app.config[Constants.STRIPE_PUBLISHABLE_KEY])
+
+
+@app.route('/dashboard', methods=['GET'])
+def page_dashboard():
+    """Dashboard page."""
+    return render_template('sorry.html')
